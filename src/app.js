6a89b702d191a530fa8324b41feaf00c471d0b4e
Jamie Turner <jamwt@convex.dev>
2023-05-01 12:17:31 -0700

    tweak tweak tweak

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 /* global instantsearch algoliasearch */
 
+// Public API Key
 const searchClient = algoliasearch(
-  'latency',
-  '6be0576ff61c053d5f9a3225e2a90f76'
+  '1KIE511890',
+  'd5802c3142d1d81cebdac1ccbb02ea9f'
 );
 
 const search = instantsearch({
-  indexName: 'instant_search',
+  indexName: 'docs',
   searchClient,
   insights: true,
 });
@@ -19,26 +20,78 @@ search.addWidgets([
     container: '#searchbox',
   }),
   instantsearch.widgets.hits({
-    container: '#hits-instant-search',
+    container: '#hits-docs',
+    cssClasses: {
+      root: 'result-stack',
+      list: 'result-list',
+      item: 'result-item',
+    },
     templates: {
       item:
-        '{{#helpers.highlight}}{ "attribute": "name" }{{/helpers.highlight}}',
+        '<strong class="item-title">{{#helpers.highlight}}{ "attribute": "title" }{{/helpers.highlight}}</strong><br> {{#helpers.snippet}}{ "attribute": "contents" }{{/helpers.snippet}}',
     },
   }),
-  instantsearch.widgets
-    .index({ indexName: 'instant_search_price_desc' })
-    .addWidgets([
-      instantsearch.widgets.configure({
-        hitsPerPage: 16,
-      }),
-      instantsearch.widgets.hits({
-        container: '#hits-instant-search-price-desc',
-        templates: {
-          item:
-            '{{#helpers.highlight}}{ "attribute": "name" }{{/helpers.highlight}}',
+  instantsearch.widgets.index({ indexName: 'stack' }).addWidgets([
+    instantsearch.widgets.configure({
+      hitsPerPage: 16,
+    }),
+    instantsearch.widgets.hits({
+      container: '#hits-stack',
+      cssClasses: {
+        root: 'result-stack',
+        list: 'result-list',
+        item: 'result-item',
+      },
+      templates: {
+        item:
+          '<strong class="item-title">{{#helpers.highlight}}{ "attribute": "title" }{{/helpers.highlight}}</strong><br> {{#helpers.snippet}}{ "attribute": "content" }{{/helpers.snippet}}',
+      },
+    }),
+  ]),
+  instantsearch.widgets.index({ indexName: 'discord' }).addWidgets([
+    instantsearch.widgets.configure({
+      hitsPerPage: 16,
+    }),
+    instantsearch.widgets.hits({
+      container: '#hits-discord',
+      cssClasses: {
+        root: 'result-stack',
+        list: 'result-list',
+        item: 'result-item',
+      },
+      templates: {
+        item(hit, { html, components }) {
+          console.log(hit);
+          return html`
+            <strong class="item-title">
+              ${components.Highlight({ hit, attribute: 'title' })}</strong
+            ><br />
+            ${hit.messages.map(
+              m =>
+                html`
+                  <p>
+                    <img src="${m.author.avatar}" class="avatar" />
+                    <strong class="author-block">
+                      ${m.author.name}
+                      ${m.author.convexer &&
+                        html`
+                          <img
+                            src="https://static.convex.dev/logo/convex-logomark-1024.png"
+                            class="convex-logo"
+                          />
+                        `}
+                      :
+                    </strong>
+                    ${m.body}
+                  </p>
+                `
+            )}
+          `;
+          // {{#helpers.highlight}}{ "attribute": "title" }{{/helpers.highlight}}</strong><br> {{#helpers.highlight}}{ "attribute": "messages" }{{/helpers.highlight}}',
         },
-      }),
-    ]),
+      },
+    }),
+  ]),
 ]);
 
 search.start();
